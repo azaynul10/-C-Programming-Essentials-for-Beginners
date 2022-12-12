70deb7f15610994eb7da796705134425754fcf82
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2022-12-13 01:26:34 +0600

    Create data_types.cpp

---
+++ data_types.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+bool x = True;
+bool isWeatherRainy = False;
+
+cout<< x <<endl;
+cout<< isWeatherRainy <<endl;
+
+int y = 5128;
+
+float pi = 3.14159265;
+double pi = 3.14159265;
+
+cout<< y<<endl;
+cout<< setprecision(10)<<pi<<endl;
+cout<< setprecsion(10)<<pi_d<<endl;
+
+//Char
+char letter = 'A';
+char dollar = '$';
+cout << letter <<endl;
+cout << dollar <<endl;
+
+return 0;
+}
