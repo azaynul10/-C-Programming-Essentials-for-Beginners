ec77ac9e60fcf2dcf1aac123d946a2f29089b6ac
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-01-03 15:37:08 +0600

    Create size_of_function.cpp

---
+++ size_of_function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int x;
+  cout << sizeof(x) << endl;
+  cout << sizeof(int) << endl;
+  
+  cout << "Char "  << sizeof(char) << endl;
+  cout << "Bool "  << sizeof(bool) << endl;
+  cout << "Float " << sizeof(float) <<endl;
+  cout << "Double "  << sizeof(double) <<endl;
+  
+  return 0;
+}
