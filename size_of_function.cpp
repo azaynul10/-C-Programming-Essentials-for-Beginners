261b61faa276f7046c1e19d10738d5b98450bf81
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-01-10 16:23:56 +0600

    Update size_of_function.cpp

--- size_of_function.cpp
+++ size_of_function.cpp
@@ -11,5 +11,12 @@ int main(){
   cout << "Float " << sizeof(float) <<endl;
   cout << "Double "  << sizeof(double) <<endl;
   
+  char bucket = 'A';
+  cout << bucket << endl;
+  
+  int mybucket = bucket;
+  cout << mybucket << endl;
+  
+  
   return 0;
 }
