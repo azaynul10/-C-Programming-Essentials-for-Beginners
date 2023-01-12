c4b8435d454ad8387c8225160fd4010d900f7935
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-01-12 17:16:41 +0600

    Create Find_the_sum.cpp

---
+++ Find_the_sum.cpp
@@ -0,0 +1,23 @@
+Find the sum
+Write a program that reads two numbers X and Y and prints their arithmetic sum.
+
+Input
+
+5
+4
+
+Output
+9
+
+#include <iostream>
+using namespace std;
+
+
+void mainFn() {
+        // Read & Write using cin,cout
+        int a;
+        int b;
+        cin >> a;
+        cin >> b;
+        cout<< a+b;
+}
