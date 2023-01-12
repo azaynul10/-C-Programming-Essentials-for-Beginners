22023b91d80bf1f07deaf2231cc984606782ecc0
Zaynul Abedin Miah <111334392+azaynul10@users.noreply.github.com>
2023-01-12 19:55:32 +0600

    Update Find_the_average.cpp

--- Find_the_average.cpp
+++ Find_the_average.cpp
@@ -1,1 +1,16 @@
 
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+
+void mainFn() {
+        float a,b,c,d,e;
+        // Read the numbers using float datatype
+        cin >> a >> b >> c >> d >> e;
+        float sum = a + b + c + d + e;
+        // Note: to print the answer accurately upto two places of decimal,
+        //       use the set precision() method of iomanip library in C++ as shown
+        
+        cout << fixed << setprecision(2) << sum/5 ;
+}
